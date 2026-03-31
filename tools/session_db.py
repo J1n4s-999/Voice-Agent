7ac74854400a35010c3185cf31becf12f420db92
Jonas Hahn <j.hahn@reinhausen.com>
2026-03-31 13:49:05 +0200

    tests

--- tools/session_db.py
+++ tools/session_db.py
@@ -1,4 +1,4 @@
-from tools.session_webhook import Webhook as WH
+from tools.session_webhook import WebhookHandler as WH
 import psycopg2
 import os
 
@@ -8,12 +8,14 @@ class DB():
         self.cursor = self.conn.cursor()
 
     def send_db(self):
-        val = WH.get_webhook()
-        self.cursor.execute("""
-        INSERT INTO appointments (name, email, appointment_date, duration_minutes, status, confirmation_token)
-        VALUES (%s, %s, %s, %s, %s, %s)
-        """, (val[0], val[1], val[2], val[3], "pending"))
-        self.conn.commit()
+        val = WH.get_data()
+        # self.cursor.execute("""
+        # INSERT INTO appointments (name, email, appointment_date, duration_minutes, status, confirmation_token)
+        # VALUES (%s, %s, %s, %s, %s, %s)
+        # """, (val[0], val[1], val[2], val[3], "pending"))
+        # self.conn.commit()
+
+        print(val[0], val[1], val[2], val[3])
 
     
 
