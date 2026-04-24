0d6adc8a8ceee52659b7a63dd607776942bb4945
Jonas <hahnjonas62@gmail.com>
2026-04-24 14:45:51 +0200

    new verify

--- app/security.py
+++ app/security.py
@@ -30,8 +30,8 @@ def get_token_expiry() -> datetime:
 
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 
 def verify_password(password: str, hashed: str) -> bool:
-    return pwd_context.verify(password, hashed)
+    return pwd_context.verify(password[:72], hashed)
